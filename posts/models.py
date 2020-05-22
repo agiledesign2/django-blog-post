123bcc314ba17ff8d925e7dca1ba53b3755bc51a
AgileDesign2 <agiledesign2.0@gmail.com>
2020-05-22 05:31:09 -0400

    edit some comments

--- posts/models.py
+++ posts/models.py
@@ -80,44 +80,35 @@ class PostQuerySet(models.QuerySet):
 class Post(models.Model):
     STATUS_DRAFT = 1
     STATUS_PUBLISHED = 2
-    #STATUS_ARCHIVED = 3
+    STATUS_ARCHIVED = 3
     STATUS = (
         (STATUS_DRAFT, 'Draft'),
         (STATUS_PUBLISHED, 'Published'),
-        #(STATUS_ARCHIVED, 'Archived'),
+        #(STATUS_ARCHIVED, 'Archived'), # no show for now
     )
-    #STATUS = (("DRAFT", "Draft"), ("PUBLISHED", "Published"))
-    #STATE_CHOICES = PINAX_BLOG_STATE_CHOICES
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="autors",
         verbose_name=_("Author"),
         on_delete=models.CASCADE
     )
-    #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(_("Title"), max_length=300)
-    category = models.ManyToManyField(Category,_("Category"), blank=False)
-    #category = models.ForeignKey(Category, blank=True, null=True, verbose_name='Category', on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category,_("Category"), blank=False, on_delete=models.CASCADE)
     slug = models.SlugField(max_length=255, unique_for_date="published")
-    #slug = models.SlugField(max_length=255, unique=True)
     content = MarkdownxField()  # markdownx
     #content = RichTextField()
     #content = MDTextField(verbose_name='Content')
     description = models.TextField(_("Description"), max_length=150, help_text="Enter you description text here.")
-    #description = models.TextField(max_length=2000, help_text="Enter you blog text here.")
     #cover = models.CharField(max_length=200, default='https://image.3001.net/images/20200304/15832956271308.jpg', verbose_name='Cover')
     created = models.DateTimeField(_("Created"), default=timezone.now, editable=False)  # when first revision was created
     updated = models.DateTimeField(_("Updated"), null=True, blank=True)  # when last revision was created (even if not published)
     #published = models.DateTimeField(_("Published"), auto_now=True)  # when last published
     published = models.DateTimeField(_("Published"), null=True, blank=True)  # when last published
-    #published_date = models.DateTimeField(auto_now=True)
     #is_recommend = models.BooleanField(default=False, verbose_name='Is Recommend')
     allow_comments = models.BooleanField(default=True)
     #status = models.CharField(default="DRAFT", choices=STATUS, max_length=10)
-    status = models.SmallIntegerField(_("State"), choices=STATUS)
-    #status = models.IntegerField(_("State"), choices=STATE_CHOICES, default=STATE_CHOICES[0][0])
+    status = models.SmallIntegerField(_("State"), choices=STATUS)	# default=STATE_CHOICES[0][0]
     views_count = models.IntegerField(_("View count"), default=0, editable=False)
-    #click_count = models.IntegerField(default=0, verbose_name='Click Count')
     #tag = models.ManyToManyField(Tag, verbose_name='Tag')
 
     # tags mechanism
